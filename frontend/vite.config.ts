f6812cdcad5c9866ad2383a572f69b2b98944e45
Scott Brown <scott.brown@compator.com>
2025-06-12 17:49:30 +0100

    fix: amend vite config to move application base to ~/ rather than ~/app/

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/app/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(new URL(".", import.meta.url).pathname, "./src"),
